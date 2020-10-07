a7660d3acf9121b5e0849cbd46fa46212dc6bd07
mfvitale <mvitale86@gmail.com>
2020-10-07 19:12:23 +0200

    Add test to get 100% mutation coverage

--- src/test/java/me/mfvitale/mutation/PalindromeTest.java
+++ src/test/java/me/mfvitale/mutation/PalindromeTest.java
@@ -3,6 +3,7 @@ package me.mfvitale.mutation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PalindromeTest {
@@ -12,5 +13,26 @@ public class PalindromeTest {
     public void when_pal() {
         Palindrome fb = new Palindrome();
         assertTrue(fb.isPalindrome("noon"));
+    }
+
+    @Test
+    @DisplayName("Passing empty string to isPalindrome must return true")
+    public void when_empty_pal() {
+        Palindrome fb = new Palindrome();
+        assertTrue(fb.isPalindrome(""));
+    }
+
+    @Test
+    @DisplayName("Passing mario string to isPalindrome must return false")
+    public void when_not_pal() {
+        Palindrome fb = new Palindrome();
+        assertFalse(fb.isPalindrome("mario"));
+    }
+
+    @Test
+    @DisplayName("Passing neon string to isPalindrome must return false")
+    public void whenNearPalindrom_thanReject(){
+        Palindrome palindromeTester = new Palindrome();
+        assertFalse(palindromeTester.isPalindrome("neon"));
     }
 }
